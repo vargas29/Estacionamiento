ee4745d0c63865a76a469ff29129b3ef7dfc2826
Maria Fernanda Mendoza Vargas - Ceiba Software <maria.mendoza@ceiba.com.co>
2019-07-18 08:35:15 -0500

    first commit

---
+++ src/main/java/com/Estacionamiento/Estacionamiento/EstacionamientoApplication.java
@@ -0,0 +1,13 @@
+package com.Estacionamiento.Estacionamiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Estacionamiento/Estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Estacionamiento.Estacionamiento;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstacionamientoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
